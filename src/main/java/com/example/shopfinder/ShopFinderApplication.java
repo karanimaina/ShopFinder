6858a746a27c95c4b247218e394a2d222e57cabb
felixmaina <karani.maina2010@gmail.com>
2022-08-17 16:42:08 +0300

    iitialize the project

---
+++ src/main/java/com/example/shopfinder/ShopFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shopfinder/ShopFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
